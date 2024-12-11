47cba3f1e079430dc0372c77541a2c5c6990b706
Shahid Khan <khan.shahidljpit2020@gmail.com>
2024-12-12 02:36:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,15 @@ app = Flask(__name__)
 CORS(app)
 application = app
 
+cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME')
+api_key = os.getenv('CLOUDINARY_API_KEY')
+api_secret = os.getenv('CLOUDINARY_API_SECRET')
 
+cloudinary.config(
+    cloud_name=cloud_name,
+    api_key=api_key,
+    api_secret=api_secret
+)
 
 @app.route('/<name>')
 def home(name):
